741c1440d825ee1765f1fbef64fe81498602ca6d
Nisha R K <55803996+nishark90@users.noreply.github.com>
2025-03-02 20:10:13 +0530

    Add project files.

---
+++ ConsoleApp2 if else statement feb 25/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2_if_else_statement_feb_25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num%2 == 0)
+            {
+                Console.WriteLine("Even");
+            }
+            else
+            {
+                Console.WriteLine("odd");
+            }
+            Console.ReadKey();
+        }
+    }
+}
